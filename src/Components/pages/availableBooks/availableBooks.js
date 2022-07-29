2fe993d702d48c6e9b045d37ee7a5036d4d4d154
inahoi <ioan.nahoi@endava.com>
2022-07-29 10:33:48 +0300

    added search bars

--- src/Components/pages/availableBooks/availableBooks.js
+++ src/Components/pages/availableBooks/availableBooks.js
@@ -31,6 +31,8 @@ export default function Availablebooks() {
   }, []);
   const [period, setPeriod] = React.useState("");
   const [title, setTitle] = React.useState("");
+  const [querry, setQuerry] = useState("");
+
   const handleSubmit = (title) => {
     setTitle(title);
     BorrowUseForm(period, title);
@@ -41,7 +43,7 @@ export default function Availablebooks() {
   };
   const settings = {
     dots: true,
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 3,
@@ -49,58 +51,69 @@ export default function Availablebooks() {
   return (
     <div>
       <NavBarLogged />
+      <h1>Available books</h1>
+      <input
+        type="text"
+        placeholder="Search by title"
+        className="search"
+        onChange={(e) => setQuerry(e.target.value)}
+      />
       <div>
         <Slider {...settings}>
-          {books.map((e) => (
-            <Card sx={{ maxWidth: 400 }} key={e.id}>
-              <CardMedia
-                component="img"
-                alt="book"
-                height="140"
-                image="https://www.incimages.com/uploaded_files/image/1920x1080/shutterstock_549881917_361412.jpg"
-              />
-              <CardContent>
-                <Typography
-                  value={title}
-                  gutterBottom
-                  variant="h5"
-                  component="div"
-                >
-                  {e.title}
-                </Typography>
-                <Typography variant="h7" component="div">
-                  {"Author: " + e.author}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {"Description:\n" + e.description}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button
-                  onClick={() => handleSubmit(e.title)}
-                  size="small"
-                  key={e.id}
-                >
-                  Borrow
-                </Button>
-                <FormControl sx={{ minWidth: 100, maxHeight: 50 }}>
-                  <InputLabel>Period</InputLabel>
-                  <Select
-                    labelId="period"
-                    id="period"
-                    value={period}
-                    label="Days to borrow"
-                    onChange={handleChange}
+          {books
+            .filter((book) =>
+              book.title.toLowerCase().includes(querry.toLowerCase())
+            )
+            .map((e) => (
+              <Card sx={{ maxWidth: 400 }} key={e.id}>
+                <CardMedia
+                  component="img"
+                  alt="book"
+                  height="140"
+                  image="https://www.incimages.com/uploaded_files/image/1920x1080/shutterstock_549881917_361412.jpg"
+                />
+                <CardContent>
+                  <Typography
+                    value={title}
+                    gutterBottom
+                    variant="h5"
+                    component="div"
                   >
-                    <MenuItem value={7}>7</MenuItem>
-                    <MenuItem value={14}>14</MenuItem>
-                    <MenuItem value={21}>21</MenuItem>
-                    <MenuItem value={30}>30</MenuItem>
-                  </Select>
-                </FormControl>
-              </CardActions>
-            </Card>
-          ))}
+                    {e.title}
+                  </Typography>
+                  <Typography variant="h7" component="div">
+                    {"Author: " + e.author}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {"Description:\n" + e.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    onClick={() => handleSubmit(e.title)}
+                    size="small"
+                    key={e.id}
+                  >
+                    Borrow
+                  </Button>
+                  <FormControl sx={{ minWidth: 100, maxHeight: 50 }}>
+                    <InputLabel>Period</InputLabel>
+                    <Select
+                      labelId="period"
+                      id="period"
+                      value={period}
+                      label="Days to borrow"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={7}>7</MenuItem>
+                      <MenuItem value={14}>14</MenuItem>
+                      <MenuItem value={21}>21</MenuItem>
+                      <MenuItem value={30}>30</MenuItem>
+                    </Select>
+                  </FormControl>
+                </CardActions>
+              </Card>
+            ))}
         </Slider>
       </div>
     </div>
